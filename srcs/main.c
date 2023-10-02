d472254597b0a95b402bcabe6f47b94b281ed475
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-10-02 19:27:42 +0200

    bring new

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: motoko <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 16:50:31 by motoko            #+#    #+#             */
-/*   Updated: 2023/10/02 19:06:08 by motoko           ###   ########.fr       */
+/*   Updated: 2023/10/02 19:18:54 by motoko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	*routine(void *lst)
 	t_list	*philo;
 
 	philo = (t_list *)lst;
-	pthread_mutex_lock(&(philo->fork_r));
+	pthread_mutex_lock(philo->fork_r);
 	printf("philo %d\n", philo->philo_id);
-	pthread_mutex_unlock(&(philo->fork_r));
+	pthread_mutex_unlock(philo->fork_r);
 }
 
 pthread_t	create_threads(t_list **tab)
@@ -54,22 +54,47 @@ pthread_t	join_threads(t_list **tab)
 	return (0);
 }
 
+/*
 int	assign_mutex(t_list *new)
 {
 	pthread_mutex_t	fork_r;
 	int		is_error;
 
-	is_error = pthread_mutex_init(&(new->fork_r), NULL);
+	is_error = pthread_mutex_init(&fork_r, NULL);
 	if (is_error)
 		handle_error("error : pthread_mutex_init\n");
+	new->fork_r = &fork_r;
 	return (0);
 }
+
+t_list	*ft_lstnew(int id)
+{
+	t_list	*new;
+	pthread_mutex_t	fork;
+	int	is_error;
+
+	is_error = pthread_mutex_init(&fork, NULL);	
+	if (is_error)
+		handle_error("phtread_init");
+
+	new = (t_list *)ft_calloc(sizeof(t_list), 1);
+	if (!new)
+		return (NULL);
+	new->philo_id = id;
+	new->fork = fork;
+	new->next = NULL;
+	return (new);
+}
+*/
 
 int	create_tab(t_list **tab, int size)
 {
 	t_list		*new;
 	int		i;
+	pthread_mutex_t	fork_r;
 
+
+	pthread_mutex_init(&fork_r, NULL);	
 	i = 0;
 	while (i < size)
 	{
@@ -78,7 +103,8 @@ int	create_tab(t_list **tab, int size)
 			return (0);
 		new->philo_id = i + 1;
 		new->meal_nb = 5;
-		assign_mutex(tab[i]);
+		new->fork_r = &fork_r;
+	//	assign_mutex(tab[i]);
 		tab[i] = new;
 		i++;
 	}
@@ -93,7 +119,7 @@ int	destroy_mutex(t_list **tab)
 	i = 0;
 	while (tab[i])
 	{
-		pthread_mutex_destroy(&(tab[i]->fork_r));
+		pthread_mutex_destroy(tab[i]->fork_r);
 		i++;
 	}
 	return (0);
@@ -105,10 +131,10 @@ int	main(int argc, char **argv)
 
 	tab = ft_calloc(5, sizeof(t_list));
 	create_tab(tab, 4);
-	//print_tab(tab);
+	print_tab(tab);
 	create_threads(tab);
 	join_threads(tab);
-	//destroy_mutex(tab);
+	destroy_mutex(tab);
 	free_tab((void *)tab);
 	return (0);
 }
